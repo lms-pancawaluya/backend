2ae69f450503ae9a7acef4f203aadeb9d5a9ad97
syamm55 <isalsolihin1234@gmail.com>
2026-09-22 14:45:20 +0700

    fix: update helpdesk category without rtl

--- src/modules/helpdesk/helpdesk.constants.js
+++ src/modules/helpdesk/helpdesk.constants.js
@@ -5,7 +5,6 @@ const TICKET_CATEGORIES = [
   { value: 'materi_konten', label: 'Materi & Konten Pembelajaran' },
   { value: 'evaluasi_kuis', label: 'Pre-Test, Post-Test & Mini Quiz' },
   { value: 'sertifikat', label: 'Sertifikat' },
-  { value: 'tugas_rtl', label: 'Tugas & RTL (Rencana Tindak Lanjut)' },
   { value: 'kendala_teknis', label: 'Kendala Teknis & Error System' },
   { value: 'lainnya', label: 'Lainnya' }
 ]
